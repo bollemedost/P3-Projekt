1659cf6625228e6abf912e22925b51ede762221c
bollemedost <iluck23@student.aau.dk>
2024-11-14 10:21:36 +0100

    Main Menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // This method is linked to the "New Game" button
+    public void NewGame()
+    {
+        // Delete all saved data in PlayerPrefs
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save(); // Make sure the deletion is saved
+
+        Debug.Log("Game progress has been reset.");
+
+        // Load the Level Map scene
+        SceneManager.LoadScene(0); // 0 is the build index of the first scene
+    }
+
+    // Method to resume the game without resetting progress
+    public void ResumeGame()
+    {
+        Debug.Log("Resuming game from the last saved state.");
+
+        // Load the Level Map or the last unlocked level
+        SceneManager.LoadScene(0); // Replace with your level map or hub scene
+    }
+}
